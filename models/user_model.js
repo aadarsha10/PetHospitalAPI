e9673be883bd82dee7928c3e6599fa0b357cba68
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-01-23 11:44:50 +0545

    Added validation and password encryption in user registration.

--- models/user_model.js
+++ models/user_model.js
@@ -2,23 +2,37 @@ const mongoose=require('mongoose')
 
 const user=mongoose.model('User',{
     firstName:{
-        type:String
+        type:String,
+        required:true 
     },
     lastName:{
-        type:String
+        type:String,
+        required:true,
+      
     },
 
     dob:{
-        type:String 
+        type:Date,
+        required:true 
      },
     userName:{
         type:String,
+        unique:true,
+        required:true,
+        
     },
     email:{
-        type:String
+        type:String,
+        unique:true,
+        required:true,
+        lowercase:true,
+        uppercase:true
+        
     },
     password:{
-        type:String
+        type:String,
+        unique:true,
+        required:true 
     }
     
 })
--- route/user_route.js
+++ route/user_route.js
@@ -1,8 +1,22 @@
 const express=require('express')
 const router=express.Router()
 const user=require('../models/user_model')
+const {check,validationResult}=require('express-validator') //for validation npm i express-validator --save
+const bcryptjs=require('bcryptjs')   //for encryption, done after validation
 
-router.post('/registerUser',function(req,res){
+// var validator = require("email-validator");
+// validator.validate("test@email.com");
+
+router.post('/registerUser',[
+    check('firstName',"First name is required!").not().isEmpty(),  //empty checking
+    check('email',"Invalid Email Address!").isEmail(),     //email check
+    check('password',"Password is required!").not().isEmpty(),
+
+],function(req,res){
+    const errors=validationResult(req)
+    
+
+    if(errors.isEmpty()){        //if there is no error
     const firstName=req.body.firstName
     const lastName=req.body.lastName
     const dob=req.body.dob
@@ -10,8 +24,16 @@ router.post('/registerUser',function(req,res){
     const email=req.body.email
     const password=req.body.password
 
-    const u1=new user({firstName:firstName,lastName:lastName,dob:dob,userName:userName,email:email,password:password}) //first ko userName vnya database ko second ko chei mathi variable
-    u1.save()
+    bcryptjs.hash(password,10,function(err,hash){   //hash varifies that a file/data hasnot altered.
+        const u1=new user({firstName:firstName,lastName:lastName,dob:dob,userName:userName,email:email,password:hash}) //first ko userName vnya database ko second ko chei mathi variable
+        u1.save()
+    })
+   
+    }
+    else{
+        res.send(errors.array())   //if there is error send errors
+    }
+   
 })
 
 router.delete('/deleteUser/:id',function(req,res){
