9ef7be34f89736a588d061aadcf40eadb2bebe93
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-04-18 07:11:59 +0545

    update added to pet

--- route/booking_route.js
+++ route/booking_route.js
@@ -6,8 +6,6 @@ const booking = require('../models/booking_model')// pet model location (auto im
 //for inserting Booking details
 
 router.post('/booking/user',function(req, res){
-    
-
     const petname=req.body.petname
     const petcondition=req.body.pcondition
     const phone=req.body.phone
@@ -16,14 +14,14 @@ router.post('/booking/user',function(req, res){
     const reason=req.body.reason
     const usrUsername=req.body.usrUserName
 
-    const bookingdata=new booking({
-        petname:petname,
-        petcondition:petcondition,
-        phone:phone,
-        date:date,
+    const bookingdata= new booking({
+        petname: petname,
+        petcondition: petcondition,
+        phone: phone,
+        date: date,
         time : time,
-        reason:reason,
-        userName:usrUsername
+        reason: reason,
+        userName: usrUsername
         })//params for pet data insertion into database
     
         bookingdata.save().then(function(){
@@ -37,13 +35,22 @@ router.post('/booking/user',function(req, res){
 
 })
 router.post('/booking/update/user', function(req,res){
-    const uname = req.body.username  
+    const petname=req.body.petname
+    const petcondition=req.body.pcondition
+    const phone=req.body.phone
+    const date=req.body.date
+    const time = req.body.time
+    const reason=req.body.reason
+    const usrUsername=req.body.usrUserName 
     console.log(uname) 
-   pet.find({userName : uname}).then(function(data){
-       res.status(200).json({message:"data fetched", petData: data})
-   })
-   .catch(function(e){ 
-       res.status(500).json({ message : e})
-   })
+    booking.updateOne({userName: usrUsername},{petName:petname, petAge:petcondition, petBreed:phone, 
+        petMedicalHistory:date, petGender : time, petType : reason})
+    .then(function(){
+        console.log("updated")
+        res.status(200).json({message : "updated"})
+    })
+    .catch(function(e){
+        res.status(500).json({message : e})
+    })
 })
 module.exports=router
